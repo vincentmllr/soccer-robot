ef916c11fe94838f424d3cc2048e79a1a9071a7a
uxmzr <philipp.binder@student.kit.edu>
2020-12-16 18:11:59 +0100

    in play_offensive() rotaionswinkel und distanz berechnet

--- action.py
+++ action.py
@@ -1,16 +1,19 @@
 
 import environment
+import math
 import perception
 import support_functions
 import anki_vector
 import anki_vector.util
 import threading
 import time
-from anki_vector.util import degrees, distance_mm, speed_mmps
+from anki_vector.util import degrees, distance_mm, speed_mmps, Angle, Pose
 
 
 # args = anki_vector.util.parse_command_args()
 # with anki_vector.Robot(args.serial) as robot:
+x_goal_enemy = 2000
+y_goal_enemy = 500
 
 def play_ball(env, robot):
     '''Entscheidet, ob Vector offensiv oder defensiv spielen soll.
@@ -37,20 +40,83 @@ def play_offensive(env, robot):
     
     # Berechnung des Richtungsvektors
     x_direction = 1
-    y_direction = (500 - y_ball)/(2000 - x_ball)
+    y_direction = (y_goal_enemy - y_ball)/(x_goal_enemy - x_ball)
 
     # Normieren des Richtungsvektors
     abs_value = ((x_direction**2)+(y_direction**2))**0.5  # Betrag
     x_direction_norm = (1/abs_value)*x_direction
     y_direction_norm = (1/abs_value)*y_direction
 
-    # Berechnen der Koordinaten des Punktes 10cm hinter dem Ball, in
+    # Berechnen der Koordinaten des Punktes [2] 10cm hinter dem Ball, in
     # Verlängerung zur Schussbahn, zu dem Vector fahren soll.
-    x_vector_new = -100 * x_direction_norm + x_ball
-    y_vector_new = -100 * y_direction_norm + x_ball
-    print("Neu brechnete Position Vector: x = " + x_vector_new + " y = "+y_vector_new)
+    x_vector_pos2 = -100 * x_direction_norm + x_ball
+    y_vector_pos2 = -100 * y_direction_norm + x_ball
+    print("Neu brechnete Position Vector: x = " + x_vector_pos2 + " y = " + y_vector_pos2)
 
-    # Vector fährt zur Position
+    # Vector fährt von Positon 1 (aktuell) zur Position 2
+    '''eventuell mit go_to_pose(pose)'''
+    
+    # y_vector_pos1 = env.self.position_y
+    # x_vector_pos1 = env.self.position_x
+    # countered_leg = y_vector_pos2 - y_vector_pos1  # Gegenkathete
+    # adjacent_leg = x_vector_pos2 - x_vector_pos1  # Ankathete
+    # angle_rad_pos2 = math.atan2(countered_leg, adjacent_leg)  # Winkel zwischen x-Achse und Gerade zwischen Postion 1 und 2 (Bogenmaß)
+    # angle_deg_pos2 = math.degrees(angle_rad_pos2)  # Winkel in Grad
+
+    # # Berechenen des winkels um den sich Vector drehen muss
+    # if angle_deg_pos2 < 0:
+    #     angle_deg_pos2 = 360 + angle_deg_pos2  # eventuell Umwandlung in positiven Winkel
+    
+    # angle_deg_vector = env.self.rotation  # aktuelle Rotation des Vectors
+    # turning_angle = 360 - angle_deg_vector + angle_deg_pos2  # Berechenen des Winkels um den sich Vector drehen muss
+
+    # if turning_angle > 360:
+    #     turning_angle = turning_angle - 360  # Winkel nicht größer als 360 Grad
+
+    # if turning_angle > 180:
+    #     turning_angle = turning_angle - 360  # Drehung nicht mehr als 180 Grad
+
+    turning_angel = turning_angel_vector(env, x_vector_pos2, y_vector_pos2) # Berechenen des Winkels um den sich Vector drehen muss (Positon 1)
+    robot.behavior.turn_in_place(degrees(turning_angel))  # Vector dreht sich auf Position 1
+
+    y_vector_pos1 = env.self.position_y
+    x_vector_pos1 = env.self.position_x
+    distance_p1_p2 = ((y_vector_pos2 - y_vector_pos1)**2 + (x_vector_pos2 - x_vector_pos1)**2)**0.5  # Strecke zwischen Position 1 und 2
+    robot.behavior.drive_straight(distance_mm(distance_p1_p2), speed_mmps(500)) # Vector fährt zu Position 2
+
+    turning_angel = turning_angel_vector(env, x_goal_enemy, y_goal_enemy) # Berechenen des Winkels um den sich Vector drehen muss (Position 2)
+    robot.behavior.turn_in_place(degrees(turning_angel)) # Vector dreht sich auf Position 1
+
+    shooting(env, robot)  # Vector fährt zum Ball und schießt
+
+
+
+
+def turning_angel_vector(env, endposition_x, endposition_y):
+    '''Berechnet Winkel, um den sich Vector auf der Stelle drehen muss,
+    um danach auf einer Gerade vom aktuellen Standort zur Endposition zu fahren.
+    '''
+    startpositon_y = env.self.position_y
+    startpositon_x = env.self.position_x
+    countered_leg = endposition_y - startpositon_y  # Gegenkathete
+    adjacent_leg = endposition_x - startpositon_x  # Ankathete
+    angle_rad_pos2 = math.atan2(countered_leg, adjacent_leg)  # Winkel zwischen x-Achse und Gerade zwischen Postion 1 und 2 (Bogenmaß)
+    angle_deg_pos2 = math.degrees(angle_rad_pos2)  # Winkel in Grad
+
+    # Berechenen des winkels um den sich Vector drehen muss
+    if angle_deg_pos2 < 0:
+        angle_deg_pos2 = 360 + angle_deg_pos2  # eventuell Umwandlung in positiven Winkel
+    
+    angle_deg_vector = env.self.rotation  # aktuelle Rotation des Vectors
+    turning_angle = 360 - angle_deg_vector + angle_deg_pos2  # Berechenen des Winkels um den sich Vector drehen muss
+
+    if turning_angle > 360:
+        turning_angle = turning_angle - 360  # Winkel nicht größer als 360 Grad
+    
+    if turning_angle > 180:
+        turning_angle = turning_angle - 360  # Drehung nicht mehr als 180 Grad
+
+    return turning_angle
 
 
 def shooting(env, robot):
