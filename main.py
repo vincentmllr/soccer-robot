eecfd473e3f3f991fde3be17dcd696230568f2a0
uxmzr <philipp.binder@student.kit.edu>
2021-01-07 15:39:10 +0100

    main() update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import support_functions as sf
 import perception
 import action
 import environment
+import time
 
 NAME = "Vector-N8G2"
 IP = "192.168.0.189"
@@ -49,7 +50,42 @@ def main():
         action.look_for_ball(env, robot)
         print("x-position Ende: ", env.self.position_x)
         print("y-position Ende: ", env.self.position_y)
+        time.sleep(3)
+
+def test():
+    
+    args = anki_vector.util.parse_command_args()
+    with anki_vector.Robot(args.serial) as robot:
+        with behavior.ReserveBehaviorControl():
+            env = environment.Environment(robot,
+                                        field_length_x=2000.0,
+                                        field_length_y=1000.0,
+                                        goal_width=200.0,
+                                        ball_diameter=40.0,
+                                        position_start_x=500.0,
+                                        position_start_y=500.0,
+                                        enable_environment_viewer=False)
+
+            detect_ball_Thread = threading.Thread(target=perception.detect_ball, args=[robot, env])
+            robot.behavior.set_head_angle(degrees(0))
+            robot.behavior.set_lift_height(0)
+            print("detect_ball()")
+            robot.camera.init_camera_feed()
+            detect_ball_Thread.start()
+            
+            print("Zum Starten Enter drücken")
+            input()
+            robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
+            print("40mm")
+            robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
+            print("40mm")
+            robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
+            print("40mm")
+            robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
+            print("40mm")
+            #action.shooting(env, robot)
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    test()
