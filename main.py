3484c1d1730c323603925f07a2f37c0eaf1bdcf6
Vincent Müller <uhksn@student.kit.edu>
2021-01-07 16:04:32 +0100

    BehaviourControl Fix

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ SERIAL = "008014c1"
 
 
 def main():
-    
-    args = anki_vector.util.parse_command_args()
-    with anki_vector.Robot(args.serial) as robot:
-        env = environment.Environment(robot,
-                                      field_length_x=2000.0,
-                                      field_length_y=1000.0,
-                                      goal_width=200.0,
-                                      ball_diameter=40.0,
-                                      position_start_x=500.0,
-                                      position_start_y=500.0,
-                                      enable_environment_viewer=False)
+
+    robot = anki_vector.Robot(serial=SERIAL)
+    robot.connect()
+    robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
+
+    env = environment.Environment(robot,
+                                  field_length_x=2000.0,
+                                  field_length_y=1000.0,
+                                  goal_width=200.0,
+                                  ball_diameter=40.0,
+                                  position_start_x=500.0,
+                                  position_start_y=500.0,
+                                  enable_environment_viewer=False)
+
+    with behavior.ReserveBehaviorControl(serial=SERIAL):
 
         # #+++EnvironmentViewerTestANFANG+++
         # viewer = environment.EnvironmentViewer(env)
@@ -52,19 +56,25 @@ def main():
         print("y-position Ende: ", env.self.position_y)
         time.sleep(3)
 
+        robot.disconnect()
+
+
 def test():
-    
-    args = anki_vector.util.parse_command_args()
-    with anki_vector.Robot(args.serial) as robot:
-        with behavior.ReserveBehaviorControl():
-            env = environment.Environment(robot,
-                                        field_length_x=2000.0,
-                                        field_length_y=1000.0,
-                                        goal_width=200.0,
-                                        ball_diameter=40.0,
-                                        position_start_x=500.0,
-                                        position_start_y=500.0,
-                                        enable_environment_viewer=False)
+
+    robot = anki_vector.Robot(serial=SERIAL)
+    robot.connect()
+    robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
+
+    env = environment.Environment(robot,
+                                  field_length_x=2000.0,
+                                  field_length_y=1000.0,
+                                  goal_width=200.0,
+                                  ball_diameter=40.0,
+                                  position_start_x=500.0,
+                                  position_start_y=500.0,
+                                  enable_environment_viewer=False)
+
+    with behavior.ReserveBehaviorControl(serial=SERIAL):
 
             detect_ball_Thread = threading.Thread(target=perception.detect_ball, args=[robot, env])
             robot.behavior.set_head_angle(degrees(0))
@@ -72,7 +82,7 @@ def test():
             print("detect_ball()")
             robot.camera.init_camera_feed()
             detect_ball_Thread.start()
-            
+
             print("Zum Starten Enter drücken")
             input()
             robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
@@ -83,9 +93,9 @@ def test():
             print("40mm")
             robot.behavior.drive_straight(distance_mm(40), speed_mmps(500))
             print("40mm")
-            #action.shooting(env, robot)
+            # action.shooting(env, robot)
 
 
 if __name__ == "__main__":
-    #main()
+    # main()
     test()
