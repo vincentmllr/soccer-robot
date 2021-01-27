f2676dc9b01b5ae20863ae0c03850204aa5a4f93
uxmzr <philipp.binder@student.kit.edu>
2021-01-27 11:12:23 +0100

    main() strukturiert und für spiel optimiert

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ import tkinter
 import perception
 import action
 import environment
-<<<<<<< HEAD
-import time
-=======
-
-
-
->>>>>>> f202056e4a9c158b423fc7bd9db173ed090b4f5f
 
 NAME = "Vector-N8G2"
 NAME_VINCENT = 'Vector-R7U1'
@@ -29,56 +22,12 @@ SERIAL_VINCENT = '00804ea0'
 
 def main():
 
-    robot = anki_vector.Robot(serial=SERIAL_VINCENT)
+    robot = anki_vector.Robot(serial=SERIAL)
     robot.connect()
     robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
 
     env = environment.Environment(robot,
-                                  field_length_x=1600.0,
-                                  field_length_y=1000.0,
-                                  goal_width=200.0,
-                                  ball_diameter=40.0,
-                                  position_start_x=500.0,
-                                  position_start_y=500.0)
-
-    with behavior.ReserveBehaviorControl(serial=SERIAL_VINCENT):
-
-        detect_ball_Thread = threading.Thread(target=perception.detect_ball, args=[robot, env])
-        robot.behavior.set_head_angle(degrees(0))
-        robot.behavior.set_lift_height(0)
-        print("detect_ball()")
-        robot.camera.init_camera_feed()
-        detect_ball_Thread.start()
-        env.environment_viewer.start()
-
-        print("Zum Starten Enter drücken")
-        input()
-
-        robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
-        print("Goooooo")
-        print("x-postion anfang: ", env.self.position_x)
-        print("y-postion anfang: ", env.self.position_y)
-        print("Rotation Vector: ", env.self.rotation)
-        action.look_for_ball(env, robot)
-        print("x-position Ende: ", env.self.position_x)
-        print("y-position Ende: ", env.self.position_y)
-        time.sleep(3)
-
-        robot.disconnect()
-
-
-def test():
-
-    robot = anki_vector.Robot(serial=SERIAL, behavior_control_level = ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY)
-    robot.connect()
-    robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
-
-    env = environment.Environment(robot,
-<<<<<<< HEAD
-                                  field_length_x=1600.0,
-=======
                                   field_length_x=1500.0,
->>>>>>> f202056e4a9c158b423fc7bd9db173ed090b4f5f
                                   field_length_y=1000.0,
                                   goal_width=200.0,
                                   ball_diameter=40.0,
@@ -90,23 +39,64 @@ def test():
     robot.behavior.set_lift_height(0)
     print("detect_ball()")
     robot.camera.init_camera_feed()
-    detect_ball_Thread.start()
+    detect_ball_Thread.start() # Start der Ballerkennung als Thread
+    print("Zum Starten des Enviroment Viewers Enter drücken")
+    input()
+    robot.behavior.set_head_angle(degrees(0))
     env.environment_viewer.start()
-
-
-    print("Zum Starten Enter drücken")
+    print("x-postion anfang: ", env.self.position_x)
+    print("y-postion anfang: ", env.self.position_y)
+    print("Rotation Vector: ", env.self.rotation)
+    print()
+    print("Zum Starten des Spiels Enter drücken")
     input()
     robot.behavior.set_head_angle(degrees(0))
     robot.behavior.set_lift_height(0)
+    print("ANPFIFF")
+    while True:
+        action.look_for_ball(env, robot)
+        
+    #robot.disconnect()
+
+
+def test():
+
+    robot = anki_vector.Robot(serial=SERIAL)
+    robot.connect()
+    robot.behavior.set_eye_color(0.05, 1.0)  # Augenfarbe orange
+
+    env = environment.Environment(robot,
+                                  field_length_x=1500.0,
+                                  field_length_y=1000.0,
+                                  goal_width=200.0,
+                                  ball_diameter=40.0,
+                                  position_start_x=500.0,
+                                  position_start_y=500.0)
+
+    detect_ball_Thread = threading.Thread(target=perception.detect_openCV, args=[robot, env])
+    robot.behavior.set_head_angle(degrees(0))
+    robot.behavior.set_lift_height(0)
+    print("detect_ball()")
+    robot.camera.init_camera_feed()
+    detect_ball_Thread.start() # Start des der Ballerkennung als Thread
+    print("Zum Starten des Enviroment Viewers Enter drücken")
+    input()
+    robot.behavior.set_head_angle(degrees(0))
+    env.environment_viewer.start()
+    print("x-postion anfang: ", env.self.position_x)
+    print("y-postion anfang: ", env.self.position_y)
+    print("Rotation Vector: ", env.self.rotation)
+    print()
+    print("Zum Starten des Spiels Enter drücken")
+    input()
+    robot.behavior.set_head_angle(degrees(0))
+    robot.behavior.set_lift_height(0)
+    print("ANPFIFF")
     running = True
     while running:
-        print("Goooooo")
-        print("x-postion anfang: ", env.self.position_x)
-        print("y-postion anfang: ", env.self.position_y)
-        print("Rotation Vector: ", env.self.rotation)
+        
         action.look_for_ball(env, robot)
-        print("x-position Ende: ", env.self.position_x)
-        print("y-position Ende: ", env.self.position_y)
+        
         print("zum abbrechen 0 drücken")
         i = input()
         if i == "0":
@@ -116,5 +106,5 @@ def test():
      
 
 if __name__ == "__main__":
-    # main()
-    test()
+    main()
+    #test()
